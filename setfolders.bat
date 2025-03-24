4a803d3fa917fd58331f84a124754a9dfc085002
Parallax <146978592+yungDoom@users.noreply.github.com>
2025-03-24 07:59:21 +0000

    Update setfolders.bat

--- setfolders.bat
+++ setfolders.bat
@@ -6,19 +6,34 @@ CLS
 ECHO 1. RobloxStudio
 ECHO 2. RCCService
 ECHO 3. WindowsClient
-Echo 4. Exit
+Echo 4. Custom Path
+Echo 5. Exit
 ECHO.
 
-CHOICE /C 1234 /M "Enter your choice:"
+CHOICE /C 12345 /M "Enter your choice:"
 
 :: This is where it gets your input
-IF ERRORLEVEL 3 GOTO Exit
+IF ERRORLEVEL 5 GOTO Exit
+IF ERRORLEVEL 4 GOTO CustomPath
 IF ERRORLEVEL 3 GOTO WindowsClient
 IF ERRORLEVEL 2 GOTO RCCService
 IF ERRORLEVEL 1 GOTO RobloxStudio
 
 :Exit
 exit
+GOTO End
+
+:CustomPath
+set /p in=Enter the path where you want the files to go
+xcopy C:\Trunk2016\Contribs\boost_1_56_0\stage\lib\*.*
+%in%
+    xcopy C:\Trunk2016\Contribs\VMProtectWin_2.13\lib\*.lib
+%in%
+    xcopy "C:\Trunk2016\Contribs\windows\x86\curl\curl-7.43.0\build\Win32\VC11\DLL Release - DLL OpenSSL\libcurl_a.lib" %in%
+    xcopy C:\Trunk2016\zlib\win\bin\Release\*.lib %in%
+    color 02
+    ECHO All the folders has been copied, have a good luck!
+    TIMEOUT /T 3
 GOTO End
 
 :WindowsClient
