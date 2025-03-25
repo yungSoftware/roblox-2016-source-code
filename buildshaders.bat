17c62ea09e5fc1f55aeccad7f1b4c23d1f7731e1
yungDoom <cagataybugrafan@gmail.com>
2025-03-25 17:26:36 +0300

    İmparator Hatayı Düzelttiiiii XDDDDDDDD v2

--- buildshaders.bat
+++ buildshaders.bat
@@ -1,2 +1,3 @@
 @echo off
-"Rendering\ShaderCompiler\ShaderCompiler.exe" /P "shaders" d3d9 d3d11 glsl glsl3 glsles glsles3
+"%~dp0\Rendering\ShaderCompiler\ShaderCompiler.exe" /P "%~dp0\shaders" d3d9 d3d11 glsl glsl3 glsles glsles3 && ^
+"%~dp0\Rendering\ShaderCompiler\ShaderCompiler_x64.exe" /P "%~dp0\shaders" d3d11_durango
