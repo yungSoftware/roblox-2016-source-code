aec9f3fdb47870a9cc82885ea14f237f7ef13bdc
yungDoom <cagataybugrafan@gmail.com>
2025-04-01 12:42:05 +0300

    sorry :)

--- Contribs/boost_1_56_0/build_boost.bat
+++ Contribs/boost_1_56_0/build_boost.bat
@@ -5,10 +5,10 @@ b2 stage ^
 --reconfigure ^
 --toolset=msvc-11.0 ^
 --variant=release ^
--s ZLIB_SOURCE=C:\Trunk2012\Contribs\windows\x86\zlib\zlib-1.2.8 ^
--s ZLIB_BINARY=C:\Trunk2012\Contribs\windows\x86\zlib\zlib-1.2.8\lib\release ^
---prefix=C:\Trunk2012\Contribs\boost_1_56_0 ^
---libdir=C:\Trunk2012\Contribs\boost_1_56_0\lib ^
---includedir=C:\Trunk2012\Contribs\boost_1_56_0\include
+-s ZLIB_SOURCE=C:\Trunk2016\Contribs\windows\x86\zlib\zlib-1.2.8 ^
+-s ZLIB_BINARY=C:\Trunk2016\Contribs\windows\x86\zlib\zlib-1.2.8\lib\release ^
+--prefix=C:\Trunk2016\Contribs\boost_1_56_0 ^
+--libdir=C:\Trunk2016\Contribs\boost_1_56_0\lib ^
+--includedir=C:\Trunk2016\Contribs\boost_1_56_0\include
 
 PAUSE
