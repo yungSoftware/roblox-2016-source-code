3e5ab8395f72808fc94f3a27627883859cf45e49
yungDoom <cagataybugrafan@gmail.com>
2025-04-24 21:29:07 +0300

    yayaya

--- genmd5.bat
+++ genmd5.bat
@@ -1,11 +1,11 @@
 @echo off
 for /f "skip=1 tokens=*" %%A in ('Certutil -hashfile RCCService\Win32\Release\RCCService.exe MD5') do (
-    echo RCCService: %%A
+    echo RCCService MD5: %%A
     goto :next
 )
 :next
 for /f "skip=1 tokens=*" %%A in ('Certutil -hashfile WindowsClient\Win32\Release\RobloxPlayerBeta.exe MD5') do (
-    echo RobloxPlayerBeta: %%A
+    echo RobloxPlayerBeta MD5: %%A
     goto :end
 )
 :end
