3c835846c52236907dc3a6d00111066a104a421a
yungDoom <cagataybugrafan@gmail.com>
2025-04-24 20:57:33 +0300

    useful

---
+++ getmd5.bat
@@ -0,0 +1,12 @@
+@echo off
+for /f "skip=1 tokens=*" %%A in ('Certutil -hashfile RCCService\Win32\Release\RCCService.exe MD5') do (
+    echo RCCService: %%A
+    goto :next
+)
+:next
+for /f "skip=1 tokens=*" %%A in ('Certutil -hashfile WindowsClient\Win32\Release\RobloxPlayerBeta.exe MD5') do (
+    echo RobloxPlayerBeta: %%A
+    goto :end
+)
+:end
+pause
