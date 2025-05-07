4736c49d5d88c01657e259b3b0482ae5155ea70c
yungDoom <cagataybugrafan@gmail.com>
2025-05-07 19:48:46 +0300

    ew

---
+++ cleaner.bat
@@ -0,0 +1,7 @@
+:: THIS IS FOR CLEANING USELESS FILES
+del RobloxStudio\bin\Release\RobloxStudioBeta.lib
+del RobloxStudio\bin\Release\RobloxStudioBeta.exp
+del RCCService\Win32\Release\RCCService.lib
+del RCCService\Win32\Release\RCCService.exp
+del WindowsClient\Win32\Release\RobloxPlayerBeta.lib
+del WindowsClient\Win32\Release\RobloxPlayerBeta.exp
