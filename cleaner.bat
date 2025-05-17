46f6620685f0bd01077bc58d7c9905b4dd47d7fd
yungDoom <cagataybugrafan@gmail.com>
2025-05-17 15:32:44 +0300

    yeah

--- cleaner.bat
+++ cleaner.bat
@@ -5,3 +5,5 @@ del RCCService\Win32\Release\RCCService.lib
 del RCCService\Win32\Release\RCCService.exp
 del WindowsClient\Win32\Release\RobloxPlayerBeta.lib
 del WindowsClient\Win32\Release\RobloxPlayerBeta.exp
+del CoreScriptConverter2\tool\win32\rsc.lib
+del CoreScriptConverter2\tool\win32\rsc.exp
