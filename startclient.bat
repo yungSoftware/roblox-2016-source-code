45f67f8e1a517d55cf13df5b39f774c7ef2ab996
yungDoom <cagataybugrafan@gmail.com>
2025-06-12 16:02:35 +0300

    ha

--- startclient.bat
+++ startclient.bat
@@ -1,3 +1,3 @@
 cd WindowsClient\win32\Release
-start RobloxPlayerBeta.exe --joinScriptUrl "http://roblox.com/Game/PlaceLauncher.ashx" --authenticationUrl "http://roblox.com/Login/Negotiate.ashx" --authenticationTicket "494857454146494148494a494a464941574a4649574a464a4941414146415746"
+start RobloxPlayerBeta.exe --joinScriptUrl "http://roblox.com/Game/PlaceLauncher.ashx" 
 exit
