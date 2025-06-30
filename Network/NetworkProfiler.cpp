cdf7a701d5c23a7e3c1201048ca96785ee172853
FreakyYield <212128012+FreakyYield@users.noreply.github.com>
2025-06-30 20:59:54 +0200

    Yeah yeah fuck you fuck raknet fuck facebook fuck roblox

--- Network/NetworkProfiler.cpp
+++ Network/NetworkProfiler.cpp
@@ -5,7 +5,6 @@
 #include "SQLite3Plugin/sqlite3.h"
 #include "SQLite3Plugin/SQLiteClientLoggerPlugin.h"
 #include "rbx/Debug.h"
-#include "RakNetTypes.h"
 
 namespace RBX {
 namespace Network {
@@ -48,33 +47,6 @@ bool NetworkProfiler::CanProfile()
 	}
 }
 
-void NetworkProfiler::Connect()
-{
-	if (!connected)
-	{
-		loggerPlugin = new RakNet::SQLiteClientLoggerPlugin();
-		packetizedTCP.AttachPlugin(loggerPlugin);
-		packetizedTCP.Start(0,0);
-		RakNet::SystemAddress serverAddress = packetizedTCP.Connect(networkSettings->profilerServerIp.c_str(), networkSettings->profilerServerPort, true);
-		if (serverAddress == RakNet::UNASSIGNED_SYSTEM_ADDRESS)
-		{
-			StandardOut::singleton()->printf(RBX::MESSAGE_ERROR, "Failed to connect to profiler log server.");
-			packetizedTCP.Stop();
-			packetizedTCP.DetachPlugin(loggerPlugin);
-			delete loggerPlugin;
-			networkSettings->profiling = false;
-		}
-		else
-		{
-			loggerPlugin->SetServerParameters(serverAddress, "roblox.db3");
-			connected = true;
-			if (networkSettings->profilerTimedSeconds > 0.f)
-			{
-				profilerTimer.reset();
-			}
-		}
-	}
-}
 
 void NetworkProfiler::Disconnect()
 {
@@ -95,14 +67,6 @@ NetworkProfiler* NetworkProfiler::singleton()
 	return &networkProfiler;
 }
 
-void NetworkProfiler::logPacket(const std::string& type, const RakNet::Packet* packet)
-{
-	if (CanProfile())
-	{
-		const char* profilerTag = networkSettings->profilerTag.c_str();
-		rakSqlLog("general", "tag, packetType, packetBitSize", (profilerTag, type.c_str(), packet->bitSize));
-	}
-}
 
 void NetworkProfiler::startCpuProfiling(int tag)
 {
@@ -139,80 +103,6 @@ void NetworkProfiler::outputCpuProfiling()
             }
         }
     }
-}
-
-void NetworkProfiler::startProfiling(const std::string& dataBlobName, const RakNet::BitStream* bitStream)
-{
-	if (CanProfile())
-	{
-		dataBlobStack.push_back(DataBlobInfo(dataBlobName, bitStream->GetReadOffset()));
-		if (dataBlobStack.size() > deepestLayer)
-		{
-			deepestLayer = dataBlobStack.size();
-		}
-	}
-}
-
-void NetworkProfiler::endProfiling(const std::string& dataBlobName, const RakNet::BitStream* bitStream)
-{
-	if (CanProfile())
-	{
-		DataBlobInfo dataBlobInfo = dataBlobStack.back();
-		RBXASSERT(dataBlobName == dataBlobInfo.name); // Make sure startProfiling and endProfiling are always in pairs
-		RBXASSERT(dataBlobStack.size() > 0);
-		const char* profilerTag = networkSettings->profilerTag.c_str();
-		if (dataBlobStack.size() == deepestLayer) // only log the leaf
-		{
-			if (!NetworkSettings::singleton().profilerOneColumnPerStack)
-			{
-				std::string layers = "";
-				for (std::vector<DataBlobInfo>::iterator iter = dataBlobStack.begin(); iter != dataBlobStack.end(); iter++)
-				{
-					layers+=((*iter).name + ".");
-				}
-				rakSqlLog("report", "tag, offset, bitsize, layers", (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, layers.c_str()));
-			}
-			else
-			{
-				switch (dataBlobStack.size())
-				{
-				case 1:
-					rakSqlLog("details", columnNames, (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, dataBlobStack.at(0).name.c_str(), "", "", "", "", "", "", "", ""));
-					break;
-				case 2:
-					rakSqlLog("details", columnNames, (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, dataBlobStack.at(0).name.c_str(), dataBlobStack.at(1).name.c_str(), "", "", "", "", "", "", ""));
-					break;
-				case 3:
-					rakSqlLog("details", columnNames, (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, dataBlobStack.at(0).name.c_str(), dataBlobStack.at(1).name.c_str(), dataBlobStack.at(2).name.c_str(), "", "", "", "", "", ""));
-					break;
-				case 4:
-					rakSqlLog("details", columnNames, (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, dataBlobStack.at(0).name.c_str(), dataBlobStack.at(1).name.c_str(), dataBlobStack.at(2).name.c_str(), dataBlobStack.at(3).name.c_str(), "", "", "", "", ""));
-					break;
-				case 5:
-					rakSqlLog("details", columnNames, (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, dataBlobStack.at(0).name.c_str(), dataBlobStack.at(1).name.c_str(), dataBlobStack.at(2).name.c_str(), dataBlobStack.at(3).name.c_str(), dataBlobStack.at(4).name.c_str(), "", "", "", ""));
-					break;
-				case 6:
-					rakSqlLog("details", columnNames, (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, dataBlobStack.at(0).name.c_str(), dataBlobStack.at(1).name.c_str(), dataBlobStack.at(2).name.c_str(), dataBlobStack.at(3).name.c_str(), dataBlobStack.at(4).name.c_str(), dataBlobStack.at(5).name.c_str(), "", "", ""));
-					break;
-				case 7:
-					rakSqlLog("details", columnNames, (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, dataBlobStack.at(0).name.c_str(), dataBlobStack.at(1).name.c_str(), dataBlobStack.at(2).name.c_str(), dataBlobStack.at(3).name.c_str(), dataBlobStack.at(4).name.c_str(), dataBlobStack.at(5).name.c_str(), dataBlobStack.at(6).name.c_str(), "", ""));
-					break;
-				case 8:
-					rakSqlLog("details", columnNames, (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, dataBlobStack.at(0).name.c_str(), dataBlobStack.at(1).name.c_str(), dataBlobStack.at(2).name.c_str(), dataBlobStack.at(3).name.c_str(), dataBlobStack.at(4).name.c_str(), dataBlobStack.at(5).name.c_str(), dataBlobStack.at(6).name.c_str(), dataBlobStack.at(7).name.c_str(), ""));
-					break;
-				case 9:
-					rakSqlLog("details", columnNames, (profilerTag, dataBlobInfo.bitStreamOffset, bitStream->GetReadOffset() - dataBlobInfo.bitStreamOffset, dataBlobStack.at(0).name.c_str(), dataBlobStack.at(1).name.c_str(), dataBlobStack.at(2).name.c_str(), dataBlobStack.at(3).name.c_str(), dataBlobStack.at(4).name.c_str(), dataBlobStack.at(5).name.c_str(), dataBlobStack.at(6).name.c_str(), dataBlobStack.at(7).name.c_str(), dataBlobStack.at(8).name.c_str()));
-					break;
-				default:
-					RBXASSERT(false && "Overflowing the network profiler data layer stack (9). Maybe add more?");
-				}
-			}
-		}
-		deepestLayer = dataBlobStack.size(); // this will prevent the non-leaf node to be printed
-		dataBlobStack.pop_back();
-	}
-}
-}
 }
 
 #endif
